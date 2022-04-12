846b98a856c96afb4cc99ead523399c79e8b1c2f
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-04-11 18:26:56 -0700

    bux fix

--- controllers/api/userroutes.js
+++ controllers/api/userroutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
